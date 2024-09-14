b3afe06e36550a27faa521d85c46ec1428c3a906
NjabuloBot Shiwu <nnsibanyoni15@gmail.com>
2024-09-14 07:46:17 +0000

    Inside the body of the callback function, added template literals `` which will contain the HTML content for the player cards. Inside the template literals, added an empty div with a class of "player-card".

--- script.js
+++ script.js
@@ -185,5 +185,6 @@ headCoach.textContent = coachName;
 
 const setPlayerCards = (arr = players) =>{
     playerCards.innerHTML += arr.map(({name,position,number,isCaptain,nickname}) =>{
+      `<div class="player-card"></div>`
     })
 }
