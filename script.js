8e0219d8034bf8d58cd0f0cc5654c0b448f0f767
NjabuloBot Shiwu <nnsibanyoni15@gmail.com>
2024-09-14 09:07:13 +0000

    Called the setPlayerCards function with an argument of players.filter(). Inside the filter method, addeed a callback function with a parameter called player and implicitly return player.nickname is not null.

--- script.js
+++ script.js
@@ -196,5 +196,8 @@ const setPlayerCards = (arr = players) =>{
 
 playersDropdownList.addEventListener("change",(e)=>{
   playerCards.innerHTML="";
-  switch(e.target.value){}
+  switch(e.target.value){
+    case "nickname":
+      setPlayerCards(players.filter((player)=> player.nickname !== null));
+  }
 })
